aad302c40250b80c583c823bd31c094a9f525dc0
dominick martin <dominickbmartin@gmail.com>
2022-11-30 22:33:16 -0800

    lab 03 update

--- README.md
+++ README.md
@@ -1,15 +1,7 @@
-# about-me-201d93
+# 201 LAB 03: About Me
 
-//Read ME Template
+## Author: Dominick Martin
 
-LAB - Number
-Project Name
-I build an app with a direct and powerful purpose. It does all of the things that it accomplishes well. I should describe the purpose and functionality so those that visit my README understand the app
+## Links and Resources
 
-Author: Student/Group Name
-Links and Resources
-submission PR
-Any Links you used as reference
-Reflections and Comments
-Consider including the answers to your daily journal and submission questions here
-This is also a good place to reflect on the tools and resources used and learned
+## Reflections and Comments
--- js/app.js
+++ js/app.js
@@ -6,68 +6,98 @@ console.log('Hey world hey');
 //TODO: GET USERS NAME - PROMPT - alert a greteting back to the user
 
 
-let userName = prompt ('What is your name?');
+let userName = prompt('What is your name?');
 
 alert(`Welcome to my site ${userName}! Please answer the following questions with a yes or no or y/n respone, thank you`);
 
 
-
-
 // TODO: prompt them the 5 yes or no (y/n) questions, alert if they got the answer right or wrong
 
-//Q1
+Q1
 
 let questionOneGuess = prompt('Do I have a daughter?').toLowerCase();
 
-if (questionOneGuess === 'yes' || questionOneGuess === 'y'){
-  alert ('You got it right')
-} else if (questionOneGuess === 'no' || questionOneGuess === 'n'){
+if (questionOneGuess === 'yes' || questionOneGuess === 'y') {
+  alert('You got it right')
+} else if (questionOneGuess === 'no' || questionOneGuess === 'n') {
   alert('Sorry, you got it wrong');
 }
 
-//Q2
+Q2
 
 let questionTwoGuess = prompt('Do I have two boys?').toLowerCase();
 
-if (questionTwoGuess === 'yes' || questionTwoGuess === 'y'){
-  alert ('You got it right')
-} else if (questionTwoGuess === 'no' || questionTwoGuess === 'n'){
+if (questionTwoGuess === 'yes' || questionTwoGuess === 'y') {
+  alert('You got it right')
+} else if (questionTwoGuess === 'no' || questionTwoGuess === 'n') {
   alert('Sorry, you got it wrong');
 }
 
-//Q3
+// //Q3
 
-let questionThreeGuess = prompt('Do I have seven cats?').toLowerCase();
+let questionThreeGuess = prompt('Do I have any cats?').toLowerCase();
 
-if (questionThreeGuess === 'yes' || questionThreeGuess === 'y'){
-  alert ('You got it right')
-} else if (questionThreeGuess === 'no' || questionThreeGuess === 'n'){
+if (questionThreeGuess === 'yes' || questionThreeGuess === 'y') {
+  alert('You got it right')
+} else if (questionThreeGuess === 'no' || questionThreeGuess === 'n') {
   alert('Sorry, you got it wrong');
 }
 
-//Q4
+// //Q4
 
 let questionFourGuess = prompt('Do I play the drums?').toLowerCase();
 
-if (questionFourGuess === 'yes' || questionFourGuess === 'y'){
-  alert ('You got it right')
-} else if (questionFourGuess === 'no' || questionFourGuess === 'n'){
+if (questionFourGuess === 'yes' || questionFourGuess === 'y') {
+  alert('You got it right')
+} else if (questionFourGuess === 'no' || questionFourGuess === 'n') {
   alert('Sorry, you got it wrong');
 }
 
-//Q5
+Q5
 
 let questionFiveGuess = prompt('Do I have a dog?').toLowerCase();
 
-if (questionFiveGuess === 'no' || questionFiveGuess === 'n'){
-  alert ('You got it right')
-} else if (questionFiveGuess === 'yes' || questionFiveGuess === 'y'){
+if (questionFiveGuess === 'no' || questionFiveGuess === 'n') {
+  alert('You got it right')
+} else if (questionFiveGuess === 'yes' || questionFiveGuess === 'y') {
   alert('Sorry, you got it wrong');
 }
 
-
 //TODO: Give a personlaized message at the end with thier name
 
-//let userName = prompt ('What is your name?');
+let userName = prompt('What is your name?');
 
 alert(`Thank You ${userName}! It was fun playing five questions with you. Have a nice day!`);
+
+//Q6
+
+for (let qSixguess = 0; qSixguess < 4; qSixguess++) {
+  let qSixnums = prompt(`Okay ${userName} now lets play a guessing game. How many cats do I have?`);
+  if (parseInt(qSixnums) === 7) {
+    alert('Bingo, you got it. I have seven cats!')
+    break;
+  } else if (parseInt(qSixnums) < 7) {
+    alert('Too Low');
+  } else if (parseInt(qSixnums) > 7) {
+    alert('Too High');
+  }
+}
+
+//Q7
+
+let numOfcats = 0;
+
+While(numOfcats > 6){
+  console.log('How many cats would you like to buy?');
+  numOfcats++;
+}
+
+// let numOfcats
+
+// if (questionSixnum === 'no' || questionSixnum === 'n') {
+//   alert('You got it right')
+// } else if (questionSixnum === 'yes' || questionSixnum === 'y') {
+//   alert('Sorry, you got it wrong');
+
+
+//TODO: Give a personlaized message at the end with thier name
